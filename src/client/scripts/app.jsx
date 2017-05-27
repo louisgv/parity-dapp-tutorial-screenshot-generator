bb64844ee6baab722d2c78f4b503aab5eac10d21
Joe Johans <lgvichy@gmail.com>
2017-05-27 11:32:37 -0700

    Screen shot 7 Time bond and string formatting, cluttered code

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
+import {Bond, TimeBond} from 'oo7';
+import {Rspan} from 'oo7-react';
+import {InputBond} from 'parity-reactive-ui';
+
 export class App extends React.Component {
+	constructor() {
+		super();
+		this.bond = new Bond();
+		this.time = new TimeBond();
+	}
+
 	render() {
-		return (<div>
-			Hello world!
-		</div>);
+		return (
+			<div>
+				<InputBond bond={this.bond} placeholder="Go ahead and type some text"/>
+				<Rspan style={{
+					color: this
+						.bond
+						.map((t) => t.match(/^[0-9]+$/)
+							? 'red'
+							: 'black')
+				}}>
+					{Bond
+						.all([this.bond, this.time])
+						.map(([msg, t]) => `${new Date(t)}: ${msg}`)}
+				</Rspan>
+			</div>
+		);
 	}
 }
