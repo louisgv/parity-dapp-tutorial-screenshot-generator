1979ea237b713382d4a4978f6a72b69d2b8b63e0
Joe Johans <lgvichy@gmail.com>
2017-05-29 22:26:49 -0700

    Use dynamic require for screenshot runner

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -1,32 +1,15 @@
 import React from 'react';
 
-import {Bond, TimeBond} from 'oo7';
-import {Rspan} from 'oo7-react';
-import {InputBond} from 'parity-reactive-ui';
+// NOTE: For future automation:
+// const tut = 6; // min: 1
+// const ss = 0; // min: 0
+
+const {tut, ss} = require('../../scene.json');
+
+const Snapshot = require(`./Tutorial${tut}`)[`Screenshot${ss}`];
 
 export class App extends React.Component {
-	constructor() {
-		super();
-		this.bond = new Bond();
-		this.time = new TimeBond();
-	}
-
 	render() {
-		return (
-			<div>
-				<InputBond bond={this.bond} placeholder="Go ahead and type some text"/>
-				<Rspan style={{
-					color: this
-						.bond
-						.map((t) => t.match(/^[0-9]+$/)
-							? 'red'
-							: 'black')
-				}}>
-					{Bond
-						.all([this.bond, this.time])
-						.map(([msg, t]) => `${new Date(t)}: ${msg}`)}
-				</Rspan>
-			</div>
-		);
+		return (<Snapshot/>);
 	}
 }
